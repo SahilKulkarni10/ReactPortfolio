2fe641ff7da458e482a8b2b3fbbda5769625f640
sahil kulkarni <sahilkulkarni@Sahils-MacBook-Air-4.local>
2024-12-04 19:08:50 +0530

    Updated design

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,6 @@
 import "./hero.scss";
 import { motion } from "framer-motion";
 
-
-
 const textVariants = {
   initial: {
     x: -500,
@@ -33,7 +31,7 @@ const sliderVariants = {
     x: "-220%",
     transition: {
       repeat: Infinity,
-      repeatType:"mirror",
+      repeatType: "mirror",
       duration: 20,
     },
   },
@@ -49,17 +47,21 @@ const Hero = () => {
           initial="initial"
           animate="animate"
         >
-          <motion.h2 variants={textVariants}>Hello!, I am Sahil kulkarni</motion.h2>
+          <motion.h2 variants={textVariants}>
+            Hello!, I am Sahil kulkarni
+          </motion.h2>
           <motion.h1 variants={textVariants}>
-           A Full Stack Developer and ML Enthusiast
+            A Full Stack Developer and ML Enthusiast
           </motion.h1>
+
           <motion.div variants={textVariants} className="buttons">
-            <motion.button variants={textVariants}>
+            <motion.button  variants={textVariants}>
               See the Latest Works
             </motion.button>
             <motion.button variants={textVariants}>Contact Me</motion.button>
           </motion.div>
-          <motion.img
+          
+                    <motion.img
             variants={textVariants}
             animate="scrollButton"
             src="/scroll.png"
